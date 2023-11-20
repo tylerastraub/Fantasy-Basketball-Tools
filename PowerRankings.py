b65090c09211094781f61e1335dfb47298822386
tylerastraub <tyler.straub@yotes.collegeofidaho.edu>
2023-11-20 11:34:40 -0700

    Updated ranking system, added .gitignore

--- PowerRankings.py
+++ PowerRankings.py
@@ -2,8 +2,8 @@ from espn_api.basketball import League
 import pandas as pd
 import numpy as np
 
-league = League(league_id=1946371300, year=2023)
-current_week = 4
+league = League(league_id=1946371300, year=2024)
+current_week = 5
 num_of_weeks = 4
 
 # url = "https://fantasy.espn.com/apis/v3/games/fba/seasons/" + \
@@ -155,35 +155,52 @@ class LeagueTeam:
     def __init__(self):
         self.name = ""
         self.categories = {"PTS" : 0, "REB" : 0, "AST" : 0, "STL" : 0, "BLK" : 0, "3PTM" : 0, "FGM" : 0, "FGA" : 0, "FG%" : 0, "FTM" : 0, "FTA" : 0, "FT%" : 0, "TO" : 0}
-        self.rosterSize = 0
         self.wins = 0
         self.losses = 0
+        self.ties = 0
         self.record = ""
 
 def generateCategoriesRankings():
     leagueTeams = {}
+    # populate basic team info
     for team in league.teams:
         leagueTeams[team.team_name] = LeagueTeam()
         leagueTeams[team.team_name].name = team.team_name
         leagueTeams[team.team_name].wins = team.wins
         leagueTeams[team.team_name].losses = team.losses
-        for player in team.roster:
-            try:
-                for cat in leagueTeams[team.team_name].categories:
-                    leagueTeams[team.team_name].categories[cat] += player.stats['2023_last_30']['total'][cat]
-                    leagueTeams[team.team_name].rosterSize += 1
-            except KeyError:
-                True
+        leagueTeams[team.team_name].ties = team.ties
 
+    # get category totals over last n weeks
+    for week in range(current_week - num_of_weeks, current_week):
+        box_scores = league.box_scores(week, 0, True)
+        for box_score in box_scores:
+            if box_score.home_team.team_name in leagueTeams:
+                home_team = leagueTeams[box_score.home_team.team_name]
+                for cat in home_team.categories:
+                    home_team.categories[cat] += box_score.home_stats[cat]["value"]
+            else:
+                print("Error: key not found! " + box_score.home_team.team_name)
+            if box_score.away_team.team_name in leagueTeams:
+                away_team = leagueTeams[box_score.away_team.team_name]
+                for cat in away_team.categories:
+                    away_team.categories[cat] += box_score.away_stats[cat]["value"]
+            else:
+                print("Error: key not found! " + box_score.away_team.team_name)
+
+    # fix FG% and FT% to be more readable, then add data to dataframe
     data = []
     for team in leagueTeams:
         t = leagueTeams[team]
         t.categories["FG%"] = round(leagueTeams[team].categories["FGM"] / leagueTeams[team].categories["FGA"], 4) * 100
         t.categories["FT%"] = round(leagueTeams[team].categories["FTM"] / leagueTeams[team].categories["FTA"], 4) * 100
-        record = "(" + str(t.wins) + "-" + str(t.losses) + ")"
+        if(t.ties > 0):
+            record = "(" + str(t.wins) + "-" + str(t.losses) + "-" + str(t.ties) + ")"
+        else:
+            record = "(" + str(t.wins) + "-" + str(t.losses) + ")"
         leagueTeams[team].record = record
         data.append([t.name, record, t.categories["PTS"], t.categories["REB"], t.categories["AST"], t.categories["STL"], t.categories["BLK"], t.categories["3PTM"], t.categories["FG%"], t.categories["FT%"], t.categories["TO"]])
 
+    # dataframe sorting
     df = pd.DataFrame(data, columns=["Team Name", "Record", "PTS", "REB", "AST", "STL", "BLK", "3PTM", "FG%", "FT%", "TO"])
     rankedData = []
     for team in leagueTeams:
@@ -213,40 +230,43 @@ def generateCategoriesRankings():
     rankedDf = rankedDf.sort_values(by=["T-Score"], ascending=True)
     rankedDf.index = np.arange(1, len(df) + 1)
     print("\nWeek " + str(current_week) + " Power Rankings\n (pre rank mods)")
-    print(rankedDf[["Team Name", "T-Score"]])
+    print(rankedDf)
     print()
 
     # now that we have the "objective" team rankings, we can use wins/losses to adjust these ranks
-    startWeek = 1
-    if current_week - num_of_weeks > 1:
-        startWeek = current_week - num_of_weeks
+    # below is old rank mods that adjusted based on strength of teams beat/lost to
+    # startWeek = 1
+    # if current_week - num_of_weeks > 1:
+    #     startWeek = current_week - num_of_weeks
 
-    rankMods = {}
-    rankRange = rankedDf["T-Score"].max() - rankedDf["T-Score"].min()
-    for team in rankedDf.loc[:, "Team Name"]:
-        rankMods[team] = 0
-    for i in range(num_of_weeks):
-        matchups = league.scoreboard(startWeek + i)
-        for matchup in matchups:
-            if(matchup.winner == "HOME"):
-                winner = matchup.home_team.team_name
-                loser = matchup.away_team.team_name
-            else:
-                winner = matchup.away_team.team_name
-                loser = matchup.home_team.team_name
-            
-            winnerRank = getTeamRank(winner, rankedDf)
-            loserRank = getTeamRank(loser, rankedDf)
-            rankDiff = winnerRank - loserRank
-            if rankDiff + rankRange != 0:
-                rankMods[winner] += (rankDiff + rankRange) / (rankRange * 2)
-                rankMods[loser] -= (rankDiff + rankRange) / (rankRange * 2)
+    # rankMods = {}
+    # rankRange = rankedDf["T-Score"].max() - rankedDf["T-Score"].min()
+    # for team in rankedDf.loc[:, "Team Name"]:
+    #     rankMods[team] = 0
+    # for i in range(num_of_weeks):
+    #     matchups = league.scoreboard(startWeek + i)
+    #     for matchup in matchups:
+    #         if(matchup.winner == "HOME"):
+    #             winner = matchup.home_team.team_name
+    #             loser = matchup.away_team.team_name
+    #         else:
+    #             winner = matchup.away_team.team_name
+    #             loser = matchup.home_team.team_name
+    #         winnerRank = getTeamRank(winner, rankedDf)
+    #         loserRank = getTeamRank(loser, rankedDf)
+    #         rankDiff = winnerRank - loserRank
+    #         if rankDiff + rankRange != 0:
+    #             rankMods[winner] += (rankDiff + rankRange) / (rankRange * 2)
+    #             rankMods[loser] -= (rankDiff + rankRange) / (rankRange * 2)
     
-    rankModifier = 6.0 # how much wins/losses should sway rank. if the worst team beats the best team, their rank improves by this much
+    # rankModifier = 6.0 # how much wins/losses should sway rank. if the worst team beats the best team, their rank improves by this much
+    # for index, row in rankedDf.iterrows():
+    #     oldRank = row["T-Score"]
+    #     rankedDf.at[index, "T-Score"] -= rankMods[row["Team Name"]] * rankModifier
+    #     print("Old rank for " + row["Team Name"] + ": " + str(oldRank) + ", new rank: " + str(rankedDf.at[index, "T-Score"]), ", rank modifier: " + str(rankMods[row["Team Name"]]))
+    winModifier = 2
     for index, row in rankedDf.iterrows():
-        oldRank = row["T-Score"]
-        rankedDf.at[index, "T-Score"] -= rankMods[row["Team Name"]] * rankModifier
-        print("Old rank for " + row["Team Name"] + ": " + str(oldRank) + ", new rank: " + str(rankedDf.at[index, "T-Score"]), ", rank modifier: " + str(rankMods[row["Team Name"]]))
+        rankedDf.at[index, "T-Score"] -= leagueTeams[row["Team Name"]].wins * winModifier
 
     rankedDf = rankedDf.sort_values(by=["T-Score"], ascending=True)
     rankedDf.index = np.arange(1, len(df) + 1)
